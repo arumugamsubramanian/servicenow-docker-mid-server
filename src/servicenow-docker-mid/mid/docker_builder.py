17b5e2ff9b4d9a71d4ebe7b0cd0e760e224c421d
Arumugam Subramanian <arumugam1987@gmail.com>
2023-09-18 13:51:21 +0530

    feat: servicenow docker build

---
+++ src/servicenow-docker-mid/mid/docker_builder.py
@@ -0,0 +1,108 @@
+import os
+
+import requests
+import csv
+import subprocess
+import argparse
+import zipfile
+from dotenv import load_dotenv
+import docker
+
+
+def generate_download_links(data, country, platform):
+    download_links = []
+
+    for row in data:
+        api_url, date, tags = row
+        date1 = date.split('_')[0]
+        year1 = date1.split('-')[2]
+        month1 = date1.split('-')[0]
+        day1 = date1.split('-')[1]
+        tag_parts = tags.split('-')
+        tag = '-'.join(tag_parts[1:])  # Remove "glide-" prefix
+        template = f"https://install.service-now.com/glide/distribution/builds/package/app-signed/mid-{platform}-container-recipe/{year1}/{month1}/{day1}/mid-{platform}-container-recipe.{tag}_{date}.{platform}.x86-64.zip"
+        download_links.append(template)
+
+    print(download_links)
+
+    return download_links
+
+
+def download_and_build(download_links):
+    for link in download_links:
+        # Download the package from the link
+        response = requests.get(link, stream=True)
+        if response.status_code == 200:
+            filename = link.split("/")[-1]
+            with open(filename, "wb") as f:
+                for chunk in response.iter_content(chunk_size=8192):
+                    f.write(chunk)
+            print(f"Downloaded {filename}")
+
+            # Create a folder with the same name as the zip file
+            folder_name = os.path.splitext(filename)[0]
+            os.makedirs(folder_name, exist_ok=True)
+
+            # Unzip the downloaded package into the folder
+            with zipfile.ZipFile(filename, "r") as zip_ref:
+                zip_ref.extractall(folder_name)
+
+            os.remove(filename)  # Remove the zip file after extraction
+
+            # Load the DOCKER_TAG environment variable from the .env file
+            load_dotenv(dotenv_path=f"{folder_name}/.env")
+            docker_tag = os.getenv("DOCKER_TAG")
+
+            # Run Docker command inside the extracted folder
+            print(docker_tag)
+            docker_image_name = docker_tag.split(':')[0]
+            docker_image_tag = docker_tag.split(':')[1]
+            docker_command = ["docker", "build", "-t", docker_tag, "."]
+            subprocess.run(docker_command, cwd=folder_name, check=True)
+            print(f"Docker build completed for {filename}")
+
+            push_docker_image(docker_image_name, docker_image_tag)
+
+
+def push_docker_image(docker_image_name, docker_image_tag):
+    # Set up a Docker client
+    docker_client = docker.from_env()
+    # Authenticate with the Docker registry
+    docker_username = os.getenv("DOCKER_USERNAME")
+    docker_password = os.getenv("DOCKER_PASSWORD")
+    docker_client.login(username=docker_username, password=docker_password)
+
+    # Tag the Docker image
+    docker_image = docker_client.images.get(docker_image_name+':'+docker_image_tag)
+    docker_image.tag(repository='arumugamsubramanian/mid', tag=docker_image_tag)
+
+    # Push the Docker image to the registry
+    docker_client.images.push(repository='arumugamsubramanian/mid', tag=docker_image_tag)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Download and build Docker images.")
+    parser.add_argument("country", help="The country for which to process data")
+    parser.add_argument("platform", help="The platform (windows or linux)")
+
+    args = parser.parse_args()
+    country = args.country
+    platform = args.platform
+
+    # Read CSV data from the local root path
+    csv_file = f"{country}_data.csv"
+
+    with open(csv_file, newline="") as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip header row
+        data = list(reader)
+
+    # Generate download links
+    download_links = generate_download_links(data, country, platform)
+
+    # Download and build packages
+    download_and_build(download_links)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/servicenow-releasenotes/release-notes-scraper/release_notes_scraper.py
@@ -0,0 +1,89 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from urllib.parse import urljoin
+import csv
+import argparse
+from collections import defaultdict
+
+
+def scrape_release_notes(country):
+    # Initialize a set to store unique API URLs
+    unique_api_urls = set()
+
+    # URL to the page containing the hyperlinks
+    base_url = f"https://servicenow-be-prod.servicenow.com/api/bundle/{country}-release-notes/page/release-notes" \
+               f"/available-versions.html"
+    # Send an HTTP GET request to the URL
+    response = requests.get(base_url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the HTML content of the page
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Extract hyperlinks with the pattern "/bundle/vancouver-release-notes/page/release-notes/quality/"
+        pattern = rf'\/bundle\/{country}-release-notes\/page\/release-notes\/quality\/[\w-]+\.html'
+        matches = re.findall(pattern, str(soup))
+
+        # Iterate through the unique links, prepend "/api/" before "bundle," and add to the set
+        for unique_link in matches:
+            # Add "/api/" before "bundle" in the URL
+            api_url = urljoin(base_url, unique_link.replace("/bundle/", "/api/bundle/"))
+            unique_api_urls.add(api_url)
+
+    # Initialize dictionaries to store unique build date and build tag values for each API URL
+    unique_build_dates = defaultdict(set)
+    unique_build_tags = defaultdict(set)
+
+    # Iterate through the unique API URLs and process the data
+    for api_url in unique_api_urls:
+        # Send an HTTP GET request to the API URL
+        api_response = requests.get(api_url)
+
+        # Check if the API request was successful (status code 200)
+        if api_response.status_code == 200:
+            # Parse the JSON data from the response
+            response_json = api_response.json()
+
+            # Extract lines containing "Build date:" and "Build tag:" using regular expressions
+            build_info_pattern = r'(Build date:|Build tag:)(.*?)\n'
+            build_info_matches = re.findall(build_info_pattern, response_json["topic_html"], re.DOTALL)
+
+            for match in build_info_matches:
+                key = match[0].strip()
+                value = match[1].strip()
+                # Remove HTML tags from the value
+                value = BeautifulSoup(value, "html.parser").text
+                if "Build date:" in key:
+                    unique_build_dates[api_url].add(value)
+                elif "Build tag:" in key:
+                    unique_build_tags[api_url].add(value)
+
+    return unique_api_urls, unique_build_dates, unique_build_tags
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Release Notes Scraper")
+    parser.add_argument("country", choices=["tokyo", "vancouver"], help="Select a country for scraping")
+
+    args = parser.parse_args()
+    country = args.country
+
+    unique_api_urls, unique_build_dates, unique_build_tags = scrape_release_notes(country)
+
+    # Create a CSV file for the current country
+    with open(f"{country}_data.csv", mode='w', newline='') as csv_file:
+        fieldnames = ["api_url", "date", "tags"]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+
+        # Write data to the CSV file
+        for api_url in unique_api_urls:
+            for date in unique_build_dates[api_url]:
+                for tag in unique_build_tags[api_url]:
+                    writer.writerow({"api_url": api_url, "date": date, "tags": tag})
+
+
+if __name__ == "__main__":
+    main()
